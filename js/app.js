bdedd898a93c002eb3ac37b1215f102169277cbb
Md. Abiruzzaman Molla <abiruzzaman.molla@gmail.com>
2021-01-16 11:58:23 +0600

    product language changed

--- js/app.js
+++ js/app.js
@@ -9,44 +9,44 @@ new Vue({
         products: [
             {
                 id: 1,
-                name: 'MacBook Pro (15 inch)',
-                description: 'This laptop has a super crisp Retina display. Yes, we know that it\'s overpriced...',
-                price: 2999,
+                name: 'অন্তরের রোগ ২ খন্ড',
+                description: 'অন্তরের রোগ বইটি লিখেছেন শাইখ সালেহ আল মুনাজ্জিদ হাফি., দুই খন্ডে বইটি প্রকাশ করেছে রুহামা প্রকাশনী',
+                price: 500,
                 inStock: 50
             },
             {
                 id: 2,
-                name: 'Samsung Galaxy Note 7',
-                description: 'Unlike the overpriced MacBook Pro, we\'re selling this one a bit cheap, as we heard it might explode...',
-                price: 299,
+                name: 'অন্তরের আমল - ২ খন্ড',
+                description: 'অন্তরের আমল বইটি লিখেছেন শাইখ সালেহ আল মুনাজ্জিদ হাফি., দুই খন্ডে বইটি প্রকাশ করেছে রুহামা প্রকাশনী',
+                price: 600,
                 inStock: 755
             },
             {
                 id: 3,
-                name: 'HP Officejet 5740 e-All-in-One-printer',
-                description: 'This one might not last for so long, but hey, printers never work anyways, right?',
-                price: 149,
+                name: 'রুহের চিকিৎসা',
+                description: 'ইবনে তাইমিয়্যাহ রহ. লিখিতি এই বইটি প্রকাশিত হয়েছে মাকতাবাতুল আসসালাফ থেকে ২০২১ সালের অনলাইন বই মেলায়',
+                price: 350,
                 inStock: 5
             },
             {
                 id: 4,
-                name: 'iPhone 7 cover',
-                description: 'Having problems keeping a hold of that phone, huh? Ever considered not dropping it in the first place?',
-                price: 49,
+                name: 'ইসলামি ইতিহাস - ৫ খন্ড',
+                description: 'ড. রাগেব সারজনি লিখিত ইসলামের ইতিহাস বইটি প্রকাশিত হয়েছে মাকতাবাতুল হাসান থেকে, এই বইটি মোট ৫টি খন্ডে প্রকাশিত হয়েছে।',
+                price: 1400,
                 inStock: 42
             },
             {
                 id: 5,
-                name: 'iPad Pro (9.7 inch)',
-                description: 'We heard it\'s supposed to be pretty good. At least that\'s what people say.',
-                price: 599,
+                name: 'সিরাতুন নবী সা. - ৪ খন্ড',
+                description: 'বিশুদ্ধ হাদিসের আলোকে রাসূল সা. এর সম্পূর্ণ জীবনী প্রকাশিত হয়েছে মাকতাবাতুল বায়ান থেকে মোট ৪ খন্ডে',
+                price: 1040,
                 inStock: 0
             },
             {
                 id: 6,
-                name: 'OnePlus 3 cover',
-                description: 'Does your phone spend most of its time on the ground? This cheap piece of plastic is the solution!',
-                price: 19,
+                name: 'যেমন ছিলেন তিনি সা. - ২ খন্ড',
+                description: 'যেমন ছিলেন তিনি বইটি শাইখ সালেহ আল-মুনাজ্জিদ হাফি. লিখিত একটি সিরাত যা রুহামা প্রকাশনী থেকে মোট ২ খন্ডে প্রকাশিত হয়েছে',
+                price: 900,
                 inStock: 81
             }
         ]
